093030f1602da9e9ba6d35d3859f9d9764aedb2d
Abhishek Singh <abhiarav03@gmail.com>
2025-12-13 03:22:24 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,19 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  reactStrictMode: false,  
+  /* config options here */
+};
+
+export default nextConfig;
+
+/*
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  reactStrictMode: false,  // ✅ Add this line
+  /* other config options here 
+};
+
+export default nextConfig;
+*/
